2c892cb0dc752e1470fdaceea838a339e9b0b865
Marco Nova <mnova@atex.com>
2026-07-11 15:15:45 +0200

    Add restart to deploy

--- releaseToProd.sh
+++ releaseToProd.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 ssh -t openhabian@192.168.1.61 "cd /home/openhabian/domino_hub && git pull && ./release.sh"
+TOKEN=$(cat "$(dirname "$0")/token.txt")
+curl -s -X POST -H "Authorization: Bearer $TOKEN" -H "Content-Type: application/json" \
+    "http://1bs8mxtwoevlqho3.myfritz.net:8123/api/services/homeassistant/restart"
+ echo "Home Assistant restarting..."
