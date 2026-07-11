def01abbc332c8a45eadd3b30b6357606f3d7cfc
Marco Nova <mnova@atex.com>
2026-07-11 12:43:12 +0200

    Fix release script

--- releaseToProd.sh
+++ releaseToProd.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-ssh openhabian@192.168.1.61 << EOF
-cd /home/openhabian/domino_hub
-git pull
-./release.sh
-EOF
+ssh -t openhabian@192.168.1.61 "cd /home/openhabian/domino_hub && git pull && ./release.sh"
